ee0f0261629c92f59bd6eaac6fdff06354ab0cf4
Subbrat <m.subbrat@outlook.com>
2023-11-08 00:36:58 +0530

    io

--- index.php
+++ index.php
@@ -1,28 +1,99 @@
-<?php include('db.php'); ?>
+</ /?php include('db.php'); ?>
 <!DOCTYPE html>
 <html>
+<title>The Change</title>
+<meta name="viewport" content="width=device-width, initial-scale=0.8">
+<link rel="stylesheet" href="./css.css">
+<!-- <link rel="stylesheet" href="./csss.css"> -->
+<link rel="stylesheet" href="./s6css.css">
+<style>
+    #body {
+        background-color: #184b78;
+        color: white;
+        margin: 3px;
+    }
 
-<head>
-    <meta charset="UTF-8">
-    <title>The Change</title>
-    <link rel="stylesheet" href="./s6css.css">
-    <link rel="stylesheet" href="./css.css">
-</head>
+    /* .s-container {
+    width: 85%;
+    margin-left: auto;
+    margin-right: auto;
+} */
+    /* .ppcd {
+    margin: 10px;
+    ;
+} */
+</style>
 
-<body>
-    <header>
-        <h1>Welcome to Your Website</h1>
-    </header>
-    <nav>
-    </nav>
-    <main>
-        <p>Hello, this is a basic PHP and HTML webpage.</p>
-        <?php
-        echo "Today's date is: " . date("Y-m-d");
-        ?>
-    </main>
-    <footer>
-    </footer>
+<body id="body">
+    <br>
+    <h3>Join the Change !</h3>
+    <div>
+        We promise to deliver the <span class="s-tx-red">protocol</span> and <span class="s-tx-red">execution</span>
+        you deserve, and the enhancement this framework should be
+        carried. With coordination of the student body and <span class="s-tx-red">Synergy</span> among ourselves.
+    </div>
+    <hr>
+    <div class="">
+        <h3>The Team</h3>
+        <table>
+            <tr>
+                <th>Name</th>
+                <th>Position</th>
+                <th>Batch</th>
+                <th>Department</th>
+                <th>Link</th>
+            </tr>
+            <tr>
+                <td>Chinmaya Sahu</td>
+                <td>President</td>
+                <td>2020</td>
+                <td>Chemistry</td>
+                <td><a href="#">Profile</a></td>
+            </tr>
+            <tr>
+                <td>Ranjit Patro</td>
+                <td>SCDC</td>
+                <td>2020</td>
+                <td>Mathematics</td>
+                <td><a href="#">Profile</a></td>
+            </tr>
+            <tr>
+                <td>Abi Sinan</td>
+                <td>C-SAC</td>
+                <td>2021</td>
+                <td>Biology</td>
+                <td><a href="#">Profile</a></td>
+            </tr>
+            <tr>
+                <td>Arsha Prakash</td>
+                <td>DC</td>
+                <td>2021</td>
+                <td>Biology</td>
+                <td><a href="#">Profile</a></td>
+            </tr>
+            <tr>
+                <td>Subrat Mallick</td>
+                <td>POS</td>
+                <td>2021</td>
+                <td>EES</td>
+                <td><a href="#">Profile</a></td>
+            </tr>
+            <tr>
+                <td>Sarojit Naik</td>
+                <td>S-SAC</td>
+                <td>2021</td>
+                <td>Chemistry</td>
+                <td><a href="#">Profile</a></td>
+            </tr>
+            <tr>
+                <td>Auromic Behera</td>
+                <td>Senate Representative</td>
+                <td>2021</td>
+                <td>Physics</td>
+                <td><a href="#">Profile</a></td>
+            </tr>
+        </table>
+    </div>
 </body>
 
 </html>
